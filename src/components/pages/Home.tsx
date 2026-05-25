8d584b6f8fd006b2ba932c9b6b1aa9cd8fd720d0
bibeksah <bibeksah36@gmail.com>
2026-05-26 01:27:36 +0545

    feat: implement SEOWrapper and base schema on Home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,66 +1,29 @@
-import { motion } from 'framer-motion'
-import VideoPlayer from '../common/VideoPlayer'
-import Amenities from '../sections/Hotel/Amenities'
-import GoogleReviewsWidget from '../sections/Hotel/GoogleReview'
-import LookAround from '../sections/Hotel/LookAround'
-import FindUs from '../sections/Hotel/ReachUs'
+import Hero from '../sections/Hotel/Hero';
+import About from '../sections/Hotel/About';
+import Room from '../sections/Hotel/Room';
+import Amenities from '../sections/Hotel/Amenities';
+import HotelGallery from '../sections/Hotel/HotelGallery';
+import GoogleReview from '../sections/Hotel/GoogleReview';
+import LookAround from '../sections/Hotel/LookAround';
+import ReachUs from '../sections/Hotel/ReachUs';
+
+// Added imports
+import SEOWrapper from '../common/SEOWrapper';
+import { defaultHotelSchema } from '../../config/seo';
 
 export default function Home() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-     <VideoPlayer
-        poster=""
-        className="h-full w-full opacity-90" embedId={'hdfmg7 79g0gh'}  />
-
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, margin: "-50px" }}
-        transition={{ duration: 0.4 }}
-      >
-        <LookAround />
-      </motion.div>
-
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, margin: "-50px" }}
-        transition={{ duration: 0.4 }}
-      >
-      
-      </motion.div>
-
-
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, margin: "-50px" }}
-        transition={{ duration: 0.4 }}
-      >
+    <SEOWrapper schema={defaultHotelSchema}>
+      <div className="w-full">
+        <Hero />
+        <About />
+        <Room />
         <Amenities />
-      </motion.div>
-
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, margin: "-50px" }}
-        transition={{ duration: 0.4 }}
-      >
-        <GoogleReviewsWidget />
-      </motion.div>
-
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, margin: "-50px" }}
-        transition={{ duration: 0.4 }}
-      >
-        <FindUs />
-      </motion.div>
-    </motion.div>
-  )
+        <LookAround />
+        <HotelGallery />
+        <GoogleReview />
+        <ReachUs />
+      </div>
+    </SEOWrapper>
+  );
 }
